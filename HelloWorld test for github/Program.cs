125816ae09c69a2bb8b4ebe61ef44a05e1c32a49
Mumriken-B <66306935+Mumriken-B@users.noreply.github.com>
2020-09-10 10:25:59 +0200

    Csharp projects - ITHS .NET -  Year1
    ITHS | .NET | Csharp | YH | First year

---
+++ HelloWorld test for github/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_test_for_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
