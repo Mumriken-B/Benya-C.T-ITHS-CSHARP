6b9e4d3200b7b7b1a7a3f4d0a791f9fdefe7f670
Mumriken-B <66306935+Mumriken-B@users.noreply.github.com>
2020-09-10 10:35:07 +0200

    Added "hello earth/pluto"
    changed line to earth and added pluto

--- HelloWorld test for github/Program.cs
+++ HelloWorld test for github/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,8 @@ namespace HelloWorld_test_for_github
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello earth");
+            Console.WriteLine("Hello pluto");
         }
     }
 }
